708222c9c09bed773fc6fecc0f2cd866a6f5bc43
christiaan-janssen <christiaanjanssen@outlook.com>
2025-10-29 20:28:57 +0100

    Move prompt to a list to allow for a conversation

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
-import pprint
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print("Usage: python main.py <prompt>")
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <prompt>")
-        sys.exit(1)
-
-    prompt = sys.argv[1]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=user_prompt,
     )
 
     print(response.text)

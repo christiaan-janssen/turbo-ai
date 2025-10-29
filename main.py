08e392df0540872a5fc1085f3da9f0cc0ca4df95
christiaan-janssen <christiaanjanssen@outlook.com>
2025-10-29 20:37:50 +0100

    add a verbose flag for debugging

--- main.py
+++ main.py
@@ -16,11 +16,15 @@ if len(sys.argv) < 2:
 
 user_prompt = sys.argv[1]
 
+verbose = False
+
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    verbose = True
+
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 
 def main():
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=user_prompt,
@@ -28,9 +32,10 @@ def main():
 
     print(response.text)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose == True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
